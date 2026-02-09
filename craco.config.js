1e6bb5e865017b43a9f799a00e5b35b1c62f441c
小海 <3277975910@qq.com>
2026-02-10 00:02:59 +0800

    first commit

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path =  require('path');
+
+const resolve = (dir)=>{
+    return path.resolve(__dirname,dir)
+}
+
+module.exports = {
+    webpack:{
+        alias:{
+            '@':resolve('src')
+        }
+    }
+}
