646a37bf2e238d83066d1b1ff3fd6cecbd9e9f76
小海 <3277975910@qq.com>
2026-02-10 00:07:54 +0800

    fix: 参数配置

--- craco.config.js
+++ craco.config.js
@@ -1,29 +1,30 @@
-const path =  require('path');
+const path = require('path');
 const CracoLessPlugin = require('craco-less');
 
-const resolve = (dir)=>{
-    return path.resolve(__dirname,dir)
+const resolve = (dir) => {
+    return path.resolve(__dirname, dir);
 }
 
 module.exports = {
-    webpack:{
-        alias:{
-            '@':resolve('src')
+    webpack: {
+        alias: {
+            '@': resolve('src'),
+            'components': resolve('src/components'), // 可选：添加更多别名
         }
     },
-    plugins:[
+    plugins: [
         {
-            plugin:CracoLessPlugin,
-            options:{
-                lessLoaderOptions:{
-                    lessOptions:{
-                        modifyvars:{
-                            '@primary-color':'#10A57A'
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {
+                            '@primary-color': '#10A57A'
                         },
-                        javascriptEnabled:true
+                        javascriptEnabled: true
                     }
                 }
             }
         }
-    ]   
+    ]
 }
