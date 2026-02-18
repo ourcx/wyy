4c4c6d06e35e004ab04c7899ad3aded7792ab2d5
朱鑫浩 <zhuxh17@chinatelecom.cn>
2026-02-18 14:01:04 +0800

    feat: 引入 antd

--- craco.config.js
+++ craco.config.js
@@ -10,11 +10,24 @@ module.exports = {
       components: resolve('src/components'),
     },
   },
+  babel: {
+    plugins: [
+      [
+        'import',
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+          style: true, // 加载 less，使主题定制生效
+        },
+      ],
+    ],
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
+          webpackImporter: true,
           lessOptions: {
             modifyVars: { '@primary-color': '#10A57A' },
             javascriptEnabled: true,
